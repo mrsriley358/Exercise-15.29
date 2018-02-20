773186e0554d4400c781b12ff6fa1894c42c70c1
GSWolfe97 <31828401+GSWolfe97@users.noreply.github.com>
2018-02-19 18:28:00 -0600

    Add files via upload

---
+++ src/exercise15_29/Exercise15_29.java
@@ -0,0 +1,36 @@
+/*
+ *  Program:    Exercise 15.29
+ *  Date:       2/19/18
+ *  Developer:  Garrett Wolfe
+ *  Purpose:    Display a car that moves and have the ability to speed it up or slow it down.
+ */
+package exercise15_29;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+
+public class Exercise15_29 extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+        CarPane pane = new CarPane();
+        pane.setOnMousePressed(e -> pane.pause());
+        pane.setOnMouseReleased(e -> pane.play());
+        pane.setOnKeyPressed(e -> {
+        if (e.getCode() == KeyCode.UP) {
+            pane.increaseSpeed();
+        }
+        else if (e.getCode() == KeyCode.DOWN) {
+            pane.decreaseSpeed();
+        }
+        });
+        Scene scene = new Scene(pane, 600, 100);
+        primaryStage.setTitle("Car");
+        primaryStage.setScene(scene); 
+        primaryStage.show(); 
+        pane.requestFocus();
+    }
+    public static void main(String[] args) {
+	launch(args);
+    }
+}
